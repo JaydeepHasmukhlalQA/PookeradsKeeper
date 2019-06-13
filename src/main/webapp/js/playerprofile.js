e654e251e008e91d8d87e01b4ed407b142ca560e
Jaydeep Hasmukhlal <jaydeep.hasmukhlal@academytrainee.com>
2019-06-13 13:22:47 +0100

    Changed way to retrieve all inputs on page load.

--- src/main/webapp/js/playerprofile.js
+++ src/main/webapp/js/playerprofile.js
@@ -1,20 +1,51 @@
+let fullnameHeader;
+let usernameInput;
+let firstnameInput;
+let lastnameInput;
+let winsInput;
+let winStreaksInput;
+let lossesInput;
+let lossStreakInput;
+let drawsInput;
+let fullBalledEnemyInput;
+let gotFullBalledInput;
+
 function getJsonPlayerFromSession() {
+    getAllInput();
+
     let fakeJson = '{"draws":0,"firstname":"Jaydeep","fullBalledEnemy":0,"gotFullBalled":0,"id":3,"lastname":"Hasmukhlal","loses":0,"lossStreakCounter":0,"lossStreaks":0,"username":"jaydeeph","winStreakCounter":0,"winStreaks":0,"wins":0}';
     //let player = JSON.parse(sessionStorage.getItem("playerJson"));
     let player = JSON.parse(fakeJson);
     console.log(player);
     let {firstname, lastname} = player;
 
-    document.getElementById("playerFullnameTag").innerHTML = firstname + " " + lastname;
-    document.getElementById("playerProfileForm").elements["username"].value = player.username;
-    document.getElementById("playerProfileForm").elements["firstname"].value = player.firstname;
-    document.getElementById("playerProfileForm").elements["lastname"].value = player.lastname;
-    document.getElementById("playerProfileForm").elements["wins"].value = player.wins;
-    document.getElementById("playerProfileForm").elements["winStreaks"].value = player.winStreaks;
-    document.getElementById("playerProfileForm").elements["losses"].value = player.loses;
-    document.getElementById("playerProfileForm").elements["lossStreaks"].value = player.lossStreaks;
-    document.getElementById("playerProfileForm").elements["draws"].value = player.draws;
-    document.getElementById("playerProfileForm").elements["fullBalledEnemy"].value = player.fullBalledEnemy;
-    document.getElementById("playerProfileForm").elements["gotFullBalled"].value = player.gotFullBalled;
+    fullnameHeader.innerHTML = firstname + " " + lastname;
+    usernameInput.value = player.username;
+    firstnameInput.value = player.firstname;
+    lastnameInput.value = player.lastname;
+    winsInput.value = player.wins;
+    winStreaksInput.value = player.winStreaks;
+    lossesInput.value = player.loses;
+    lossStreakInput.value = player.lossStreaks;
+    drawsInput.value = player.draws;
+    fullBalledEnemyInput.value = player.fullBalledEnemy;
+    gotFullBalledInput.value = player.gotFullBalled;
+}
+
+function getAllInput() {
+    fullnameHeader = document.getElementById("playerFullnameTag");
+    usernameInput = document.getElementById("playerProfileForm").elements["username"];
+    firstnameInput = document.getElementById("playerProfileForm").elements["firstname"];
+    lastnameInput = document.getElementById("playerProfileForm").elements["lastname"];
+    winsInput = document.getElementById("playerProfileForm").elements["wins"];
+    winStreaksInput = document.getElementById("playerProfileForm").elements["winStreaks"];
+    lossesInput = document.getElementById("playerProfileForm").elements["losses"];
+    lossStreakInput = document.getElementById("playerProfileForm").elements["lossStreaks"];
+    drawsInput = document.getElementById("playerProfileForm").elements["draws"];
+    fullBalledEnemyInput = document.getElementById("playerProfileForm").elements["fullBalledEnemy"];
+    gotFullBalledInput = document.getElementById("playerProfileForm").elements["gotFullBalled"];
+}
+
+function updateMeButtonPressed() {
 
 }
